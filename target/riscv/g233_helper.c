6fc46928fb5240cd429078cebaee52826142f166
Panda Jiang <3160104094@zju.edu.cn>
2025-10-30 22:11:38 +0800

    instructions completed
    Signed-off-by: Panda Jiang <3160104094@zju.edu.cn>

--- target/riscv/g233_helper.c
+++ target/riscv/g233_helper.c
@@ -27,22 +27,46 @@ void HELPER(dma)(CPURISCVState *env,
 }
 
 void HELPER(sort)(CPURISCVState *env,
-    target_ulong sa, target_ulong sz,
-    target_ulong da)
+    target_ulong sa, target_ulong an,
+    target_ulong sn)
 {
-
+    for (int i = 0; i < sn - 1; ++i) {
+        int swapped = 0;
+        for (int j = 0; j < sn -i -1; ++j) {
+            target_ulong src = sa + j * 4;
+            target_ulong dst = src + 4;
+            int t1 = cpu_ldl_data(env, src);
+            int t2 = cpu_ldl_data(env, dst);
+            if (t1 > t2) {
+                cpu_stl_data(env, src, t2);
+                cpu_stl_data(env, dst, t1);
+                swapped = 1;
+            }
+        }
+        if (!swapped) {
+            break;
+        }
+    }
 }
 
 void HELPER(crush)(CPURISCVState *env,
-    target_ulong sa, target_ulong sz,
-    target_ulong da)
+    target_ulong dst, target_ulong src,
+    target_ulong num)
 {
-
+    for (int i = 0; i < num; i+=2) {
+        u_int16_t data = cpu_ldsw_data(env, src + i);
+        uint8_t cd = (uint8_t)((data & 0xf) | ((data >> 8)& 0xf) << 4);
+        cpu_stb_data(env, dst + i/2, cd);
+    }
 }
 
 void HELPER(expand)(CPURISCVState *env,
-    target_ulong sa, target_ulong sz,
-    target_ulong da)
+    target_ulong dst, target_ulong src,
+    target_ulong num)
 {
-
+    for (int i = 0; i < num; i+=1) {
+        uint8_t data = cpu_ldub_data(env, src + i);
+        uint16_t cd = (uint16_t)(data & 0xf) | (uint16_t)(((data >> 4)& 0xf) << 8);
+        cpu_stw_data(env, dst + i*2, cd);
+    }
 }
